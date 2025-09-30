2f8db310bd464afc4957a2e89842842570020d28
saymuria <naumova.al2001@gmail.com>
2025-09-30 21:33:40 +0300

    Добавали использование KotlinDSL в тесты

--- src/main/kotlin/steps/UserSteps.kt
+++ src/main/kotlin/steps/UserSteps.kt
@@ -3,32 +3,28 @@ package steps
 import framework.skeleton.Endpoint
 import framework.skeleton.Endpoint.CREATE_ACCOUNT
 import framework.skeleton.Endpoint.UPDATE_CUSTOMER_PROFILE
-import framework.skeleton.requesters.CrudRequester
 import framework.skeleton.requesters.ValidatedCrudRequester
-import framework.specs.RequestSpecs
 import framework.specs.RequestSpecs.Companion.authAsUser
-import framework.specs.ResponseSpec
 import framework.specs.ResponseSpec.Companion.entityWasCreated
 import framework.specs.ResponseSpec.Companion.requestReturnOk
-import framework.specs.ResponseSpec.Companion.requestReturnsBadRequest
-import io.restassured.response.ValidatableResponse
 import models.accounts.createAccount.CreateAccountResponse
 import models.accounts.deposit.DepositMoneyRequest
 import models.accounts.deposit.DepositMoneyResponse
 import models.accounts.transfer.TransferMoneyRequest
 import models.accounts.transfer.TransferMoneyResponse
 import models.customer.updateCustomerProfile.UpdateCustomerProfileRequest
+import models.customer.updateCustomerProfile.UpdateCustomerProfileResponse
 
-class UserSteps {
+class UserSteps(val username: String,val  password: String) {
 
-    fun createAccount(username: String, password: String): CreateAccountResponse {
+    fun createAccount(): CreateAccountResponse {
         return ValidatedCrudRequester<CreateAccountResponse>(
             authAsUser(username, password),
             entityWasCreated(), CREATE_ACCOUNT
         ).post(null)
     }
 
-    fun makeDeposit(username: String, password: String, request: DepositMoneyRequest): DepositMoneyResponse {
+    fun makeDeposit(request: DepositMoneyRequest): DepositMoneyResponse {
         return ValidatedCrudRequester<DepositMoneyResponse>(
             authAsUser(username, password),
             requestReturnOk(),
@@ -36,18 +32,19 @@ class UserSteps {
         ).post(request)
     }
 
-    fun makeTransfer(username: String, password: String, request: TransferMoneyRequest): TransferMoneyResponse {
+    fun makeTransfer(request: TransferMoneyRequest): TransferMoneyResponse {
         return ValidatedCrudRequester<TransferMoneyResponse>(
             authAsUser(username, password),
-            ResponseSpec.requestReturnOk(),
+            requestReturnOk(),
             Endpoint.TRANSFER_MONEY
         ).post(request)
     }
 
-    fun updateName(username: String, password: String, request: UpdateCustomerProfileRequest) : ValidatableResponse {
-       return CrudRequester(authAsUser(username, password),
-            requestReturnOk(),
-            UPDATE_CUSTOMER_PROFILE
-        ).update(null, request)
+    fun updateName(request: UpdateCustomerProfileRequest) : UpdateCustomerProfileResponse {
+       return ValidatedCrudRequester<UpdateCustomerProfileResponse>(
+           authAsUser(username, password),
+           requestReturnOk(),
+           UPDATE_CUSTOMER_PROFILE
+       ).update(null, request)
     }
 }
